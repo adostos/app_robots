2706fc5ae49ee9a3aa8f8443cffd8501404ee307
adostos <ado19960@gmail.com>
2020-07-31 19:13:45 +0200

    Updates

---
+++ precache-manifest.efbbe05fcf91c53d5a26b63a4ae1a557.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bd74930c21a3938840290a9716e7b03",
+    "url": "/app_robots/index.html"
+  },
+  {
+    "revision": "266d37d2590c8c1a67a7",
+    "url": "/app_robots/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "04f880bce9edee7d7417",
+    "url": "/app_robots/static/js/2.2015a1d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/app_robots/static/js/2.2015a1d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "266d37d2590c8c1a67a7",
+    "url": "/app_robots/static/js/main.ec760621.chunk.js"
+  },
+  {
+    "revision": "59bde47ed53174c13f71",
+    "url": "/app_robots/static/js/runtime-main.aa3fafa2.js"
+  }
+]);
